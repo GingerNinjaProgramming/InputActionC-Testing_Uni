7eae00bd2b64548a2c9e7378b5b547dcc0a5c0e7
GingerNinjaProgramming <connervian@outlook.com>
2026-03-04 13:09:26 +0000

    Boiler Plate

---
+++ InputActionTesting/Source/InputActionTesting/InputActionTesting.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InputActionTesting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InputActionTesting, "InputActionTesting" );
